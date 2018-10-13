84d8ba636485a20089767c7bcb22c75113f4118e
syahrulutomo <irullvandalis@gmail.com>
2018-10-13 16:57:24 +0700

    fix login via Google & Fb. Provide sign in and profil page (2)

--- scripts/signin.js
+++ scripts/signin.js
@@ -48,6 +48,7 @@ function signInFacebook(){
   var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
+   console.log(user);
   // ...
 }).catch(function(error) {
   // Handle Errors here.
