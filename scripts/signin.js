1ca01f6563df90b0ba10bdfc07141e579b9c8980
syahrulutomo <irullvandalis@gmail.com>
2018-10-13 16:45:21 +0700

    fix login via Google & Fb. Provide sign in and profil page

---
+++ scripts/signin.js
@@ -0,0 +1,62 @@
+var loginGoogleButton = document.querySelector('.login-google-button');
+var loginFbButton = document.querySelector('.login-fb-button');
+
+ // Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "AIzaSyB4tu8OwisUwHb-AOZSOG1_sJXGTVIyZKo",
+    authDomain: "kaledostorage.firebaseapp.com",
+    databaseURL: "https://kaledostorage.firebaseio.com",
+    projectId: "kaledostorage",
+    storageBucket: "kaledostorage.appspot.com",
+    messagingSenderId: "27410977713"
+  };
+  firebase.initializeApp(config);
+
+var providerGoogle = new firebase.auth.GoogleAuthProvider();
+var providerFacebook = new firebase.auth.FacebookAuthProvider();
+
+loginGoogleButton.addEventListener('click',signInGoogle);
+loginFbButton.addEventListener('click',signInFacebook);
+
+
+function signInGoogle(){
+	firebase.auth().signInWithPopup(providerGoogle).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  console.log(user);
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+}
+
+
+
+function signInFacebook(){
+  firebase.auth().signInWithPopup(providerFacebook).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
