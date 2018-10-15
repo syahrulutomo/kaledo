bc93e17b95d16efdcc0d49f970d9d4cc7853f02c
syahrulutomo <irullvandalis@gmail.com>
2018-10-15 20:55:42 +0700

    coba get info email dan url photos menggunakan graph api

--- scripts/signin.js
+++ scripts/signin.js
@@ -33,16 +33,20 @@ var loginFbButton = document.querySelector('.fb-login-button');
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 
+var user = new Object();
 loginFbButton.onclick = function(){
   FB.login(function(response) {
     if (response.authResponse) {
      console.log('Welcome!  Fetching your information.... ');
      console.log(response);
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-       console.log('Your email is, ' + response.email + '.');
-       console.log('Your profile pic is, ' + response.profile_pic + '.');
-     });
+     FB.api(
+      '/1930921673670327/picture',
+      'GET',
+      user,
+      function(response) {
+        console.log(resonse.data);
+      }
+    );
     } else {
      console.log('User cancelled login or did not fully authorize.');
     }
