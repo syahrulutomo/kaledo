fc3ea411fb91ba9697b88e1f3a0e9b634a78eae1
syahrulutomo <irullvandalis@gmail.com>
2018-11-09 05:31:06 +0700

    fix menu profil highlight

--- scripts/signin.js
+++ scripts/signin.js
@@ -2,16 +2,16 @@
 var email = localStorage.getItem('email');
 
 
-if(window.location.href == 'https://kaledocooking.github.io/static/signin.html' && (email !== null || email !== '')) {
+if(email !== null || email !== ''){
     window.location = 'profil.html';
-  
-}else if(window.location.href != 'https://kaledocooking.github.io/static/signin.html' && (email !== null || email !== '')){
     document.querySelector('profil-link').href = 'profil.html';
-    document.querySelector('profil-link-index').href = 'profil.html';
-}else if(window.location.href != 'https://kaledocooking.github.io/static/signin.html' && (email === null || email === '')){
+    document.querySelector('profil-link-index').href = 'profil.html';  
+}else(email === null || email === '') {
+    window.location = 'signin.html';
     document.querySelector('profil-link').href = 'signin.html';
     document.querySelector('profil-link-index').href = 'signin.html';
 }
+
 
 var loginGoogleButton = document.querySelector('.login-google-button');
 var loginFbButton = document.querySelector('.fb-login-button');
