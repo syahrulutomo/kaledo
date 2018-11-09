517c9b1a8b7e4477776d2784ebb7cda1cda7707f
syahrulutomo <irullvandalis@gmail.com>
2018-11-09 08:06:21 +0700

    conditional header index or header

--- scripts/core.js
+++ scripts/core.js
@@ -16,21 +16,24 @@ main.addEventListener('click', function() {
    drawer.classList.remove('open');
 });
 
+if(window.location.href == 'https://kaledocooking.github.io/index.html' ){
+  new Vue({
+	  el: '#header-index',
+	  data: {
+	  	email: localStorage.getItem('email')
+	  }
+  });	
+}
+else{
+  new Vue({
+  	el: '.header',
+  	data: {
+  		email: localStorage.getItem('email')
+  	}
+  });	
+}
 
 
-new Vue({
-  el: '#header-index',
-  data: {
-  	email: localStorage.getItem('email')
-  }
-});
-
-new Vue({
-  el: '.header',
-  data: {
-  	email: localStorage.getItem('email')
-  }
-});
 
 if(localStorage.getItem('photo') !== null){
 	document.querySelector('.profil-img').src = localStorage.getItem('photo');
