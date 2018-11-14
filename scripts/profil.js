f5cb43c8bc6c08f1d5b8fe93254bd3025ea4de52
syahrulutomo <irullvandalis@gmail.com>
2018-11-14 19:46:24 +0700

    upload fedit personal info done

--- scripts/profil.js
+++ scripts/profil.js
@@ -31,7 +31,7 @@ Vue.component('tab-account',{
 		<a id='delete_account' href="account-settings.html"><img src="../assets/setting.png"></a>
 		<div class="account-form account-form-top">
 			<p>Email</p>
-			<input v-model="email" id="email" type="text" name="">
+			<input v-model="email" id="email" type="text" name="" readonly>
 		</div>
 		<div class="account-form">
 			<p>First Name</p>
@@ -43,90 +43,188 @@ Vue.component('tab-account',{
 		</div>
 		<div class="account-form">
 			<img id="profil-picture" src="../assets/grey.jpg" alt="">
-			<input  id="file" type="file" class="file" placeholder="Upload Photo">
-		 	<label class="label-file" for="file">Upload Photo</label>
+			<input value="localStorage.getItem('profilPicture')" id="file" type="file" class="file" placeholder="Upload Photo">
 		</div>
-		<button v-on:click="postData()" id="submit-account">Save</button>
+		<button v-on:click="handlerClick" id="submit-account">Save</button>
 	</section>
 
 		`,
 		methods: {
 			postData: function(){
-				axios.put(`https://kaledo-backend.herokuapp.com/api/users/`+email,{
-					email: this.email,
-					firstName: this.firstName,
-					lastName: this.lastName,
-					password: this.password		
-			})
-			.then(response => {})
-			.then(function(){
-				var file =  document.querySelector("#file");
-				var selectedFile = '';
+				if(this.email !== '' && this.email !== null){
+					 if(this.firstName !== '' && this.firstName !== null){
+						if(this.lastName !== '' && this.lastName !== null){
+							var reqBody = {
+							        		email: this.email,
+							        		firstName: this.firstName,
+							        		lastName: this.lastName,
+							        		profilPicture: this.profilPicture
+							        	  };
 
-				document.querySelector('#submit-account').addEventListener('click',uploadFile);
+							if(fetch){
+								document.querySelector('.loader').style.display = "inline";
+							}
 
-				document.addEventListener('DOMContentLoaded',function() {
-				    document.querySelector('#file').onchange=changeEventHandler;
-				},false);
+							fetch('https://kaledo-backend.herokuapp.com/api/users/'+this.email, {
+					        	method: "PUT", // *GET, POST, PUT, DELETE, etc.
+						        // mode: "no-cors", // no-cors, cors, *same-origin
+						        headers: {
+						            "Content-Type": "application/json; charset=utf-8",
+						            // "Content-Type": "application/x-www-form-urlencoded",
+						        },	
+					        	body: JSON.stringify(reqBody)
+					        })
+							.then(function(response){
+								localStorage.setItem('email',reqBody['email']);
+								localStorage.setItem('firstName',reqBody['firstName']);
+								localStorage.setItem('lastName',reqBody['lastName']);
+								
+								return response;
+							})
+							.then(function(){
 
-				function changeEventHandler(event) {
-				    selectedFile = event.target.files[0];
-				    uploadButton.style.display = "inline-block";
-				    document.querySelector('.label-file').innerHTML	 = event.target.files[0].name;
-				}
+								if(document.querySelector('#file').value !== ''  || document.querySelector('#file').files[0] !== undefined){
 
+								var config = {
+								    apiKey: "AIzaSyB4tu8OwisUwHb-AOZSOG1_sJXGTVIyZKo",
+								    authDomain: "kaledostorage.firebaseapp.com",
+								    databaseURL: "https://kaledostorage.firebaseio.com",
+								    projectId: "kaledostorage",
+								    storageBucket: "kaledostorage.appspot.com",
+								    messagingSenderId: "27410977713"
+								};
+					 
+								firebase.initializeApp(config);
+						 
 
-				function uploadFile(){
-					var fileName = selectedFile.name;
+								var file =  document.querySelector("#file").files[0];
+								var fileName = file.name;
+								var id = localStorage.getItem('email'); 
+								var url = '';
+								var storageRef = firebase.storage().ref('user_'+id+'/account/'+  fileName);	
 
-					var storageRef = firebase.storage().ref('kaledo-recipe/' + fileName);	
+								var uploadTask = storageRef.put(file);
 
-					var uploadTask = storageRef.put(selectedFile);
+								// Register three observers:
+								// 1. 'state_changed' observer, called any time the state changes
+								// 2. Error observer, called on failure
+								// 3. Completion observer, called on successful completion
+								uploadTask.on('state_changed', function(snapshot){
+							  	// Observe state change events such as progress, pause, and resume
+								// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+								var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+							  	
+							  	console.log('Upload is ' + progress + '% done');
+							  	
+							  	switch (snapshot.state) {
+							    case firebase.storage.TaskState.PAUSED: // or 'paused'
+							      console.log('Upload is paused');
+							      break;
+							    case firebase.storage.TaskState.RUNNING: // or 'running'
+							      console.log('Upload is running');
+							      break;
+								  }
+								}, function(error) {
+								  // Handle unsuccessful uploads
+								}, function() {					
 
-					// Register three observers:
-					// 1. 'state_changed' observer, called any time the state changes
-					// 2. Error observer, called on failure
-					// 3. Completion observer, called on successful completion
-					uploadTask.on('state_changed', function(snapshot){
-					  // Observe state change events such as progress, pause, and resume
-					  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-					  var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-					  console.log('Upload is ' + progress + '% done');
-					  switch (snapshot.state) {
-					    case firebase.storage.TaskState.PAUSED: // or 'paused'
-					      console.log('Upload is paused');
-					      break;
-					    case firebase.storage.TaskState.RUNNING: // or 'running'
-					      console.log('Upload is running');
-					      break;
-					  }
-					}, function(error) {
-					  // Handle unsuccessful uploads
-					}, function() {
-					  // Handle successful uploads on complete
-					  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-					  uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-					  	var id = localStorage.getItem('email');
-					  	firebase.database().ref('recipe/' +id).set({
-					  		username: firstName+''+lastName,
-					  		category: 'main course',
-					  		subCategory: 'beef',
-					  		url: downloadURL
-					  	})
-					    console.log('File available at', downloadURL);
-					  });
-					});
+							  	// Handle successful uploads on complete
+							 	// For instance, get the download URL: https://firebasestorage.googleapis.com/...
+							  	uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
 
-				}
-			
+								  	var id = JSON.stringify(localStorage.getItem('email'));
+								  	id = id.replace('.','dot');
 
-			})
-			.catch(e => {
-		      this.errors.push(e)
-		    })
+								  	firebase.database().ref('users'+id+'/account/').set({
+								  		firstName: localStorage.getItem('firstName'),
+								  		lastName: localStorage.getItem('lastName'),
+								  		url: downloadURL
+								  	});
+									
+									// putRequest(downloadURL);
+									
+									localStorage.setItem('downloadURL',downloadURL);
+								    // console.log('File available at', downloadURL);
+
+								    var email = localStorage.getItem('email');
+									reqBody1 = {
+						        		email: localStorage.getItem('email'),
+						        		firstName: localStorage.getItem('firstName'),
+						        		lastName: localStorage.getItem('lastName'),
+						        		profilPicture: localStorage.getItem('downloadURL')
+						        	};
+
+									fetch('https://kaledo-backend.herokuapp.com/api/users/'+email, {
+							        	method: 'PUT',
+							        	// mode: "no-cors", // no-cors, cors, *same-origin
+								        headers: {
+								            "Content-Type": "application/json; charset=utf-8",
+								            // "Content-Type": "application/x-www-form-urlencoded",
+								        },	
+							        	body: JSON.stringify(reqBody1)
+							        })
+									.then(function(){
+										var url = localStorage.getItem('downloadURL');
+										localStorage.setItem('profilPicture',url);
+										
+										document.location.reload(true);
+										
+									});
+
+								  	});	// end of uploadTask.snapshot  
+
+									});	// end of anonymous func to handle successfull upload
+
+								}else{
+										
+									document.location.reload(true);
+								}
+								
+
+							}); // end of then
+							
+
+						} //end of if lastName 
+					} //end of if firtsName 
+				} //end of if email
+				
+			},
+			putRequest: function(downloadURL){
+
+					fetch('https://kaledo-backend.herokuapp.com/api/users', {
+			        	method: 'PUT',	
+			        	body: {
+			        		firstName: this.firstName,
+			        		lastName: this.lastName,
+			        		email: this.email,
+			        		profilPicture: downloadURL
+			        	}
+			        })
+					.then(response => response.json());
+			},
+			updateData: function(){
+				 fetch(`https://kaledo-backend.herokuapp.com/api/users/`+this.email)
+				.then(function(response){
+					response.json();	
+				})
+				.then(function(data){
+
+				console.log(data);
+				localStorage.setItem('firstName',data['firstName']);
+				localStorage.setItem('lastName',data['lastName']);
+				localStorage.setItem('profilPicture',data['profilPicture']);
+								    
+				})
+			},
+			handlerClick: function(){
+				this.postData();
+				// this.putRequest(localStorage.getItem('downloadURL'));
+				// this.updateData();
+			}					
+												
 		}
-	}
-	
+		
+		
 })
 
 Vue.component('tab-recipes', { 
